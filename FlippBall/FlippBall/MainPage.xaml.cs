6c1cecc431abdc4835dc6334eca024bae57df22f
Daníel Adolfsson <daa11@hi.is>
2017-05-06 13:34:41 +0000

    initial commit
    Static render of ball in place
    
    TODO: add physics and remove static drawing

---
+++ FlippBall/FlippBall/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using Android.Content;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace FlippBall
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+        async void OnStartClicked(object sender, EventArgs args)
+        {
+            var page = new gameActivity();
+
+            await Navigation.PushModalAsync(page);
+            //await DisplayAlert("FlippBall", "This will start the game.", "OK");
+        }
+    }
+}
---
+++ FlippBall/FlippBall/gameActivity.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using SkiaSharp.Views.Forms;
+using SkiaSharp;
+
+namespace FlippBall
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class gameActivity : ContentPage
+	{
+        public gameActivity()
+        {
+            InitializeComponent();
+            Title = "Fling";
+
+            SKCanvasView canvasView = new SKCanvasView();
+            canvasView.PaintSurface += OnCanvasViewPaintSurface;
+            Content = canvasView;
+        }
+        
+        void OnCanvasViewPaintSurface(object sender, SKPaintSurfaceEventArgs args)
+        {
+            SKImageInfo info = args.Info;
+            SKSurface surface = args.Surface;
+            SKCanvas canvas = surface.Canvas;
+
+            canvas.Clear();
+
+            SKPaint paint = new SKPaint
+            {
+                Style = SKPaintStyle.Stroke,
+                Color = Color.PaleVioletRed.ToSKColor(),
+                StrokeWidth = 5
+            };
+            canvas.DrawCircle(info.Width / 2, info.Height-50, 50, paint);
+
+        }
+    }
+}
